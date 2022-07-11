0ce46f2a67f7b3822041bd6cfc7f12a4c34eea91
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-07-11 21:36:11 +0900

    add home api

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ ca = certifi.where()
 client = MongoClient('mongodb+srv://test:sparta@cluster0.a32x0.mongodb.net/cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
