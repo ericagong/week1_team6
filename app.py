afd03631b254624f2a0785f2adb6e3877202cc32
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-07-12 13:28:45 +0900

    add menu search api

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 import certifi
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,9 +9,24 @@ client = MongoClient('mongodb+srv://test:sparta@cluster0.a32x0.mongodb.net/clust
                      tlsCAFile=ca)
 db = client.dbsparta
 
+# 검색을 위한 text 인덱스 생성
+db.ingredients.create_index([('menu', 'text')]) #메뉴
+# db.ingredients.create_index([('$**', 'text')]) #전체
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/search')
+def search():
+    menu_receive = request.args.get('menu_give')
+    menu_list = list(db.ingredients.find({'$text': {'$search': menu_receive}}, {'_id': False}))
+    print(menu_list)
+    return render_template('result.html', menu_list=menu_list)
+
+@app.route('/result')
+def result_page():
+    return render_template('result.html')
 
 @app.route('/signup')
 def signup():
